28f95601b618141704495a4cf185e89a408b52e1
fgalimberti <36402320+fgalimberti@users.noreply.github.com>
2023-05-12 16:50:10 +0200

    Update config-dist.php
    modificati il link web e la dataroot

--- config-dist.php
+++ config-dist.php
@@ -145,7 +145,7 @@ $CFG->dboptions = array(
 // If you need both intranet and Internet access please read
 // http://docs.moodle.org/en/masquerading
 
-$CFG->wwwroot   = 'http://example.com/moodle';
+$CFG->wwwroot   = 'https://moodleapp.herokuapp.com';
 
 
 //=========================================================================
@@ -161,7 +161,7 @@ $CFG->wwwroot   = 'http://example.com/moodle';
 //
 // - On Windows systems you might specify something like 'c:\moodledata'
 
-$CFG->dataroot  = '/home/example/moodledata';
+$CFG->dataroot  = '/tmp';
 
 
 //=========================================================================
